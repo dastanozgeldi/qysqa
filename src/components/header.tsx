eca67b9b8998e26f670467d7522c7ea1083307b6
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-03-23 19:26:43 +0500

    fix: spacing on right side of heading

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -38,7 +38,7 @@ export function Header() {
         )}
 
         <div className="flex items-center gap-4">
-          <div className="flex justify-end items-center p-4 gap-4 h-16">
+          <div className="flex justify-end items-center gap-4">
             <SignedOut>
               <SignInButton>
                 <Button variant="outline" size="sm">
