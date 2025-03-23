4144970599447cb0afa69d1b450b62cb15eb50d1
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-03-23 05:17:43 +0500

    feat: create links table in schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,10 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
 
-// export const usersTable = pgTable("users", {
-//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-//   name: varchar({ length: 255 }).notNull(),
-//   age: integer().notNull(),
-//   email: varchar({ length: 255 }).notNull().unique(),
-// });
+export const linksTable = pgTable("links", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  slug: varchar("pathname", { length: 256 }).notNull().unique(),
+  originalUrl: varchar("original_url", { length: 2048 }).notNull(),
+  clicks: integer("clicks").notNull().default(0),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  userId: varchar("user_id", { length: 256 }), // Optional: for Clerk authentication
+});
